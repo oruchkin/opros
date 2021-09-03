0a08669ffad66ea9896d77cf0e227dbaa98c2758
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-09-03 22:42:08 +0300

    createsuperuser

--- opros/urls.py
+++ opros/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('app.urls'))
+    path('hello/', include('app.urls')),
+    path('api/', include('rest_framework.urls')),
 ]
